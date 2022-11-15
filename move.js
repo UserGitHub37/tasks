57c0364c318632efc43be8778282aaa311c61a78
UserGitHub37 <bum37@mail.ru>
2022-11-16 00:40:19 +0300

    chore: add move task

---
+++ move.js
@@ -0,0 +1,18 @@
+// Terminal game move function
+// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+// Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+// Самое сложное здесь это понять описание.
+// Суть такая:
+// Игрок начинает с заданной позиции и перемещается на удвоенное значение броска кубиков.
+// Рассчитать конечное положение.
+
+function move (position, roll) {
+  return position + roll * 2;
+}
+
+
+console.log(move(0, 4), 8);
+console.log(move(3, 6), 15);
+console.log(move(2, 5), 12);
