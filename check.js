6daf7c2ba70ecae6bd5fdc46bf1c55de934b1dea
UserGitHub37 <bum37@mail.ru>
2022-11-18 21:10:58 +0300

    chore: add check task

---
+++ check.js
@@ -0,0 +1,16 @@
+// *** No Loops Allowed ***
+
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. x can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+// Looking for more, loop-restrained fun? Check out the other kata in the series:
+
+function check(a,x){
+  return a.some(item => item === x);
+};
+
+console.log(check([66, 101], 66), true);
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
+console.log(check(['t', 'e', 's', 't'], 'e'), true);
+console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
