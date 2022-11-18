c982e4b382e6abcb2a2568f42a13a5c6cee7bf8f
UserGitHub37 <bum37@mail.ru>
2022-11-18 22:35:56 +0300

    chore: add balancedNum task

--- check.js
+++ check.js
@@ -14,3 +14,10 @@ console.log(check([66, 101], 66), true);
 console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45), true);
 console.log(check(['t', 'e', 's', 't'], 'e'), true);
 console.log(check(['what', 'a', 'great', 'kata'], 'kat'), false);
+
+
+//! Best Practice))
+
+// function check(a, x) {
+//   return a.includes(x);
+// }
