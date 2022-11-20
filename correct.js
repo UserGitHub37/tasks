6189ac2973299fa88f4ad1423297e776d3e5b660
UserGitHub37 <bum37@mail.ru>
2022-11-21 02:14:35 +0300

    chore: add greetDevelopers task

--- correct.js
+++ correct.js
@@ -35,3 +35,20 @@ console.log("BUDAPEST");
 console.log('---------------');
 console.log(correct("PAR15"),);
 console.log("PARIS");
+
+// ! Best Practice
+// const corrections = {
+//   '5': 'S',
+//   '0': 'O',
+//   '1': 'I',
+// };
+
+// const correct = string => (
+//   string.replace(/[501]/g, character => corrections[character])
+// );
+
+// function correct(string) {
+//   return string.replace(/0/g, "O")
+//                .replace(/5/g, "S")
+//                .replace(/1/g, "I");
+// }
