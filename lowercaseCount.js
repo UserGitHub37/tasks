5fd603847fd43831116ef62da56567117ecdd6e6
UserGitHub37 <bum37@mail.ru>
2022-12-09 21:32:07 +0300

    chore: add 'lowercaseCount' task

---
+++ lowercaseCount.js
@@ -0,0 +1,28 @@
+// Regex count lowercase letters
+
+// Your task is simply to count the total number of lowercase letters in a string.
+
+// Examples
+// lowercaseCount("abc"); ===> 3
+
+// lowercaseCount("abcABC123"); ===> 3
+
+// lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 3
+
+// lowercaseCount(""); ===> 0;
+
+// lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"); ===> 0
+
+// lowercaseCount("abcdefghijklmnopqrstuvwxyz"); ===> 26
+
+
+function lowercaseCount(str){
+  return str.match(/[a-z]/g)?.length || 0;
+}
+
+console.log(lowercaseCount("abc"), 3);
+console.log(lowercaseCount("abcABC123"), 3);
+console.log(lowercaseCount("abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"), 3);
+console.log(lowercaseCount(""), 0)
+console.log(lowercaseCount("ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"), 0)
+console.log(lowercaseCount("abcdefghijklmnopqrstuvwxyz"), 26);
