6a4bc9923ca52cce9189e172228828ed32ce16d5
UserGitHub37 <bum37@mail.ru>
2023-01-09 12:02:30 +0300

    chore: add 'Greed is Good' task

---
+++ greedIsGood.js
@@ -0,0 +1,83 @@
+// Greed is Good
+
+// Greed is a dice game played with five six-sided dice. Your mission, should you choose to accept it, is to score a throw according to these rules. You will always be given an array with five six-sided dice values.
+
+// Three 1's => 1000 points
+// Three 6's =>  600 points
+// Three 5's =>  500 points
+// Three 4's =>  400 points
+// Three 3's =>  300 points
+// Three 2's =>  200 points
+// One   1   =>  100 points
+// One   5   =>   50 point
+// A single die can only be counted once in each roll. For example, a given "5" can only count as part of a triplet (contributing to the 500 points) or as a single 50 points, but not both in the same roll.
+
+// Example scoring
+
+// Throw       Score
+// ---------   ------------------
+// 5 1 3 4 1   250:  50 (for the 5) + 2 * 100 (for the 1s)
+// 1 1 1 3 1   1100: 1000 (for three 1s) + 100 (for the other 1)
+// 2 4 4 5 4   450:  400 (for three 4s) + 50 (for the 5)
+// In some languages, it is possible to mutate the input to the function. This is something that you should never do. If you mutate the input, you will not be able to pass all the tests.
+
+function score(dice) {
+  let sum = 0;
+  const obj = dice.reduce((acc, cur) => {
+    acc[cur] ? acc[cur]++ : acc[cur] = 1;
+    return acc;
+  }, {});
+
+  for (const num in obj) {
+    switch (num) {
+      case '1':
+        switch (obj[num]) {
+          case 1: sum += 100;
+            break;
+          case 2: sum += 200;
+            break;
+          case 3: sum += 1000;
+            break;
+          case 4: sum += 1100;
+            break;
+          case 5: sum += 1200;
+            break;
+        }
+        break;
+
+      case '5':
+        switch (obj[num]) {
+          case 1: sum += 50;
+            break;
+          case 2: sum += 100;
+            break;
+        }
+
+      default:
+        if (obj[num] >= 3) sum += num * 100;
+        break;
+    }
+  }
+
+  return sum;
+}
+
+
+console.log(score( [2, 3, 4, 6, 2] ), "Should be 0" );
+console.log(score( [4, 4, 4, 3, 3] ), "Should be 400" );
+console.log(score( [2, 4, 4, 5, 4] ), "Should be 450" );
+console.log(score( [ 1, 1, 1, 3, 3 ] ), "Should be 1000" );
+console.log(score( [ 1, 1, 1, 1, 3 ] ), "Should be 1100" );
+console.log(score( [ 5, 5, 5, 3, 3 ] ), "Should be 500" );
+
+
+//! Best Practice
+// function score( dice ) {
+//   var dc = [0,0,0,0,0,0];
+//   var tdr = [1000,200,300,400,500,600];
+//   var sdr = [100,0,0,0,50,0];
+//   dice.forEach(function(x){ dc[x-1]++; });
+//   return dc.reduce(function(s,x,i){
+//     return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
+//   },0);
+// }
