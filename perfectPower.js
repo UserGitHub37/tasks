83921cb2d09c0e055948d5f1b20862013617e8b4
UserGitHub37 <bum37@mail.ru>
2023-01-30 13:09:43 +0300

    chore: add 'Perfect Power' task

---
+++ perfectPower.js
@@ -0,0 +1,37 @@
+// What's a Perfect Power anyway?
+
+// A perfect power is a classification of positive integers:
+
+// In mathematics, a perfect power is a positive integer that can be expressed as an integer power of another positive integer. More formally, n is a perfect power if there exist natural numbers m > 1, and k > 1 such that mk = n.
+
+// Your task is to check wheter a given integer is a perfect power. If it is a perfect power, return a pair m and k with mk = n as a proof. Otherwise return Nothing, Nil, null, NULL, None or your language's equivalent.
+
+// Note: For a perfect power, there might be several pairs. For example 81 = 3^4 = 9^2, so (3,4) and (9,2) are valid solutions. However, the tests take care of this, so if a number is a perfect power, return any pair that proves it.
+
+// Examples
+// Test.describe("perfect powers", function(){
+//   Test.it("should work for some examples",function(){
+//     Test.assertSimilar(isPP(4), [2,2], "4 = 2^2");
+//     Test.assertSimilar(isPP(9), [3,2], "9 = 3^2");
+//     Test.assertEquals( isPP(5), null, "5 isn't a perfect number");
+//   });
+// });
+
+
+function isPP (n) {
+  for (let i = 2; i <= n / 2; i++) {
+    let num = i;
+    for (let j = 2; j <= n / 2; j++) {
+      num *= i;
+      if (num > n) break;
+      if (num === n) return [i, j];
+    }
+  }
+  return null;
+}
+
+
+console.log(isPP(4), [2,2], "4 = 2^2");
+console.log(isPP(9), [3,2], "9 = 3^2");
+console.log(isPP(5), null, "5 isn't a perfect number");
+console.log(isPP(118636749824), [ 164, 5 ], "118636749824 = 164^5");
